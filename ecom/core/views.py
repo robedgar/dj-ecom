e3f199ac440d09a9ea8654ea0ca2cd9bb879ef63
Rob Edgar <info@robertedgar.co.uk>
2023-10-28 20:29:01 +0100

    Added product schema

--- ecom/core/views.py
+++ ecom/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 
 def frontpage(request):
-    return render(request, 'core/base.html')
+    products = Product.objects.all()[0:8]
+
+    return render(request, "core/frontpage.html", {"products": products})
